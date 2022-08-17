74c767f891ae79e9a6c039f3fa4aaf70f056d6cd
emperatriz108 <emperatriz108@gmail.com>
2022-08-17 12:22:45 -0600

    Subiendo cambios

---
+++ index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">    <link rel="stylesheet" href="assets/css/formu_leds.css">
+    <link rel="stylesheet" href="css/principal.css">
+<title>EcuPlay</title>
+
+</head>
+
+<nav class=" right navbar navbar-expand-lg bg-light ">
+  <div class="container-fluid right" >
+  <a class="navbar-brand" href="#">
+      <img src="img/edu.png" alt="" width="30" height="24">
+    </a>
+    <a class="navbar-brand" href="#">EcuPlay</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse alineado" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#inicio">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#ben">Beneficios</a>
+        </li>
+      </ul>
+
+    </div>
+  </div>
+</nav>
+<body>
+
+<div class="t1">
+    <h1>Implementacion Aplicacion Movil</h1>
+    <h2>Fomente el aprendizaje de los niños con una aplicación móvil interactiva</h2>
+</div>
+<div id="inicio" class="s1">
+<p>La educación es una parte fundamental para el desarrollo de una persona, es un proceso constante de adquisición de conocimientos 
+    desde nuestros primeros años como aprender a leer, escribir y relacionar. Las Instituciones Educativas han utilizado recursos 
+    como libros, cuadernos de ejercicios, tareas extracurriculares, actividades de aula, entre otros recursos para la enseñanza. 
+    Sin embargo, algunos niños podrían tener dificultades para entender o comprender temáticas impartidas en clase. 
+    A medida que pasa el tiempo las tecnologías han ido evolucionando y forman parte de nuestro día a día, es una herramienta que la 
+    mayoría de las personas utilizan para diversas actividades o entretenimiento, entre esos usuarios son los niños que los usan 
+    como medio de entretenimiento. A partir de esa situación se plantea la implementación de una aplicación móvil enfocada en la 
+    Educación Inicial y Parvularia y Educación Básica para promover el aprendizaje interactivo distribuido en las diferentes 
+    asignaturas que se presente según el nivel académico. 
+<br><br>
+Esta aplicación se enfocaría en realizar diversas actividades entretenidas e interactivas para los niños donde se presenta un 
+recurso educativo de una materia expresado en un juego dinámico según la temática del recuso. Estos juegos o actividades van a 
+ser ponderadas o basadas en la puntuación para analizar el rendimiento y proceso de aprendizaje del estudiante. 
+<br><br>
+El docente podrá ver el proceso del estudiante según el resultado de los datos de puntuación a través de su usuario correspondiente, 
+estos datos brindaran una mejor idea del proceso de aprendizaje de los estudiantes y analizar en que temáticas podría reforzarse.
+</p>
+
+<img src="img/edu.png" alt="" width="600" height="600">
+</div>
+<div id="ben" class="s2">
+<h2>Beneficios</h2>
+<div>
+
+    <h3>1.	Enseña nuevos métodos de aprendizaje</h3>
+    <p>	Al ser una aplicación para la enseñanza los estudiantes aprenderán por medio de diferentes 
+        actividades interactivas y entretenidas.</p>
+</div>
+<div>
+    
+<h3>2.	Reporte sobre el proceso de estudiantes</h3>
+    <p>	Se podrá generar un reporte detallado 
+        del proceso de los estudiantes a través de la aplicación móvil</p>
+</div>
+</div>
+
+<div class="s3">
+   
+    <div>
+    <h3>Contribucion de cliente</h3>
+        <h4>Brindar datos</h4>
+        <p>Tendrá que brindar acceso a los datos de los estudiantes</p>
+
+        <h4>Brindar materiar didactico</h4>
+        <p>Tendrá que brindar material didáctico para implementarlo en la aplicacion</p>
+    
+    </div>
+
+</div>
+<div class="diagram">
+<h3>Diagrama de Arquitectura</h3>
+    <img src="img/diagrama.png" alt="Diagrama Arquitectua EduPlay">
+</div>
+
+<div class="consultor">
+    
+    <img src="img/consultor.jpg" alt="Gema Manzano Consultora">
+    <div>
+    <p>
+        Gema Emperatriz Manzano Machuca <br>
+        Estudiante de Ingeniería en Sistemas Informáticos<br><br>
+        </h3>
+        <h4>Áreas en que quiera especializarme:</h4>
+       
+        1. Desarrollo y diseño web <br>
+        2. Desarrollo de aplicaciones moviles <br>
+
+        
+
+    </p>
+    </div>
+
+    <div>
+    <h4>Conocimientos Tecnicos</h4>
+        <ul>
+            <li>Java</li>
+            <li>PHP</li>
+            <li>MySQL</li>
+            <li>PostgreSQL</li>
+            <li>HTML, CSS, JS</li>
+            <li>Microsoft Word</li>
+            <li>Microsoft Power Point</li>
+            <li>Microsoft Excel</li>
+        </ul>
+        <br>
+    </div>
+    <div>
+                
+       <h4>Otros Conocimientos:</h4>
+        <ul>
+            <li>Idioma Ingles Básico</li>
+            <li>Photoshop Basico</li>
+        </ul>
+    </div>
+</div>
+
+
+
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</body>
+<footer></footer>
+</html>
