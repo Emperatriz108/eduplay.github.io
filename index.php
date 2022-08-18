47403e5095e63a71ee857827da5eb8d237f71170
Gema Emperatriz Manzano Machuca <43799487+Emperatriz108@users.noreply.github.com>
2022-08-17 21:06:13 -0600

    Update index.php
    Actualizacion

--- index.php
+++ index.php
@@ -6,8 +6,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">    <link rel="stylesheet" href="assets/css/formu_leds.css">
     <link rel="stylesheet" href="css/principal.css">
 <title>EduPlay</title>
-
-</head>
+    </head>
 
 <nav class=" right navbar navbar-expand-lg bg-light ">
   <div class="container-fluid right" >
