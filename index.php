51c3581c0c8902d883629a4f0c4b183e4f03240b
emperatriz108 <emperatriz108@gmail.com>
2022-08-17 21:10:36 -0600

    Modificacion de nav

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">    <link rel="stylesheet" href="assets/css/formu_leds.css">
     <link rel="stylesheet" href="css/principal.css">
-<title>EduPlay</title>
-
+    <title>EduPlay</title>
 </head>
 
+<body>
 <nav class=" right navbar navbar-expand-lg bg-light ">
   <div class="container-fluid right" >
   <a class="navbar-brand" href="">
@@ -31,8 +31,6 @@
     </div>
   </div>
 </nav>
-<body>
-
 <div class="t1">
     <h1>Implementacion Aplicacion Movil</h1>
     <h2>Fomente el aprendizaje de los niños con una aplicación móvil interactiva</h2>
